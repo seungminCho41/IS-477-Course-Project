8504aca2d450040c5c8543f8d1e9c08a45fe247d
Seungmin Cho <candor4141@gmail.com>
2025-12-09 21:46:32 -0600

    Update run_all.sh to execute notebooks
    Added commands to execute final_notebook and Visualizations notebooks.

--- workflow/run_all.sh
+++ workflow/run_all.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
+# Move to the project root directory
 cd "$(dirname "$0")/.."
 
+# Execute final_notebook notebook
 jupyter nbconvert --to notebook --execute notebooks/final_notebook.ipynb --inplace
+
+# Execute Visualizations notebook
+jupyter nbconvert --to notebook --execute notebooks/Visualizations.ipynb --inplace
